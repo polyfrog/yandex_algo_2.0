cd4c741e1e4c95f4e4f0c921df4e5b6dc70e68fe
polyfrog <79518107+polyfrog@users.noreply.github.com>
2021-09-07 23:24:21 +0300

    Create d.py

---
+++ hw2/d.py
@@ -0,0 +1,9 @@
+# https://contest.yandex.ru/contest/28738/problems/D/
+
+def benches(l, k, blocks):
+    pass
+
+
+l, k = map(int, input().split())
+blocks = list(map(int, input().split()))
+print(benches(l, k, blocks))
