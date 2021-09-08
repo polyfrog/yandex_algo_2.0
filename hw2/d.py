38d351a7a0bc396f49f51f81b53f95afa2a31240
polyfrog <79518107+polyfrog@users.noreply.github.com>
2021-09-08 14:46:31 +0300

    добавлено решение

--- hw2/d.py
+++ hw2/d.py
@@ -1,9 +1,17 @@
 # https://contest.yandex.ru/contest/28738/problems/D/
 
 def benches(l, k, blocks):
-    pass
+    middle = l // 2
+    closest_l = blocks[0]
+    for i in range(len(blocks)):
+        if l % 2 == 1 and blocks[i] == middle:
+            return [middle]
+        elif blocks[i] < middle:
+            closest_l = blocks[i]
+        elif blocks[i] >= middle:
+            return closest_l, blocks[i]
 
 
 l, k = map(int, input().split())
 blocks = list(map(int, input().split()))
-print(benches(l, k, blocks))
+print(*benches(l, k, blocks))
